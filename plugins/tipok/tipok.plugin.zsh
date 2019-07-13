317bfe910819f9c1a8a81fcccc5d1db5f99adfd4
Anton Marker (anmarker) <anmarker@cisco.com>
2019-07-13 12:55:04 +0200

    Update tipok.plugin.zsh
    added new alias

--- plugins/tipok/tipok.plugin.zsh
+++ plugins/tipok/tipok.plugin.zsh
@@ -1,1 +1,3 @@
 alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs -nw"
+
+alias kubernetes_dashboard_token="kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep kubernetes-dashboard-token | awk '{print $1}')"
