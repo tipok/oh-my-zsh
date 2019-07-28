f616c7e16b6cbbb5ad45c12c2c87e4302f98187f
Anton Marker (anmarker) <anmarker@cisco.com>
2019-07-28 10:47:50 +0200

    Update tipok.plugin.zsh
    improved kubernetes function

--- plugins/tipok/tipok.plugin.zsh
+++ plugins/tipok/tipok.plugin.zsh
@@ -1,3 +1,6 @@
 alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs -nw"
 
-alias kubernetes_dashboard_token="kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep kubernetes-dashboard-token | awk '{print $1}')"
+function kubernetes_dashboard_token () {
+    kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep kubernetes-dashboard-token | awk '{print $1}')
+}
+
