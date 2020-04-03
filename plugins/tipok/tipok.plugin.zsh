422b33dffff7cfce37c3bde638d99f4790560169
Anton Marker (anmarker) <anmarker@cisco.com>
2020-04-03 15:17:36 +0200

    Added git diff function

--- plugins/tipok/tipok.plugin.zsh
+++ plugins/tipok/tipok.plugin.zsh
@@ -4,3 +4,8 @@ function kubernetes_dashboard_token () {
     kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep kubernetes-dashboard-token | awk '{print $1}')
 }
 
+function git_diff () {
+    local branch_to_compare=$1
+    local current_branch=$(git rev-parse --abbrev-ref HEAD)
+    git diff $branch_to_compare..$current_branch
+}
