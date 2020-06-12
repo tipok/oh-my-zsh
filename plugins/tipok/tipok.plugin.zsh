edddd2aef7a01eb0be34d0ce71ae284c915b7d29
Anton Marker (anmarker) <anmarker@cisco.com>
2020-06-12 15:41:40 +0200

    added helper function

--- plugins/tipok/tipok.plugin.zsh
+++ plugins/tipok/tipok.plugin.zsh
@@ -9,3 +9,9 @@ function git_diff () {
     local current_branch=$(git rev-parse --abbrev-ref HEAD)
     git diff $branch_to_compare..$current_branch
 }
+
+function mov2gif () {
+    local mov_file=$1
+    local output_file=${2:-'out.gif'}
+    ffmpeg -i $mov_file -pix_fmt rgb24 -r 10 -f gif - | gifsicle --optimize=3 --delay=3 > $output_file
+}
